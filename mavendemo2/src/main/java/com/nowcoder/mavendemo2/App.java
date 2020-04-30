3b43d4372d230162b22b90b8a995c9a2d70d17ca
wangdongc <13549214017@163.com>
2020-04-30 10:42:01 +0800

    message

---
+++ mavendemo2/src/main/java/com/nowcoder/mavendemo2/App.java
@@ -0,0 +1,14 @@
+package com.nowcoder.mavendemo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//1
+        System.out.println( "Hello World!" );
+    }
+}
